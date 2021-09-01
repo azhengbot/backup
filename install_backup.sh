37ee0d173c64bcff2245f04c4aaf5484b168b425
xuhaoqing <xuhaoqing@lattebank.com>
2021-09-01 15:05:50 +0800

    Backup on: 2021年 9月 1日 星期三 15时05分50秒 CST

--- install_backup.sh
+++ install_backup.sh
@@ -12,7 +12,7 @@ install_to_crontab() {
         echo
         echo SHELL=$SHELL > crontab_conf
         echo PATH=$PATH >> crontab_conf
-        echo "00 12 * * * $script > /tmp/dotfile_Backup.log && osascript -e 'display notification \"$WD/backup\" with title \"dotfile备份成功, 已上传至 GitHub\" '" >> crontab_conf
+        echo "5 15 * * * $script > /tmp/dotfile_Backup.log && osascript -e 'display notification \"$WD/backup\" with title \"dotfile备份成功, 已上传至 GitHub\" '" >> crontab_conf
         echo "运行 \$ crontab -e"
 
         echo "插入以下内容: "
@@ -24,13 +24,11 @@ install_to_crontab() {
         crontab -l > crontab_conf
         echo SHELL=$SHELL >> crontab_conf
         echo PATH=$PATH >> crontab_conf
-        echo "* 15 * * * $script > /tmp/dotfile_Backup.log && osascript -e 'display notification \"$WD/backup\" with title \"dotfile备份成功, 已上传至 GitHub\" '" >> crontab_conf
+        echo "5 15 * * * $script > /tmp/dotfile_Backup.log && osascript -e 'display notification \"$WD/backup\" with title \"dotfile备份成功, 已上传至 GitHub\" '" >> crontab_conf
         crontab crontab_conf && rm -f crontab_conf
 
         echo "🎉 自动化备份dotfile安装完成"
-        echo
         echo "目前的 crontab 配置"
-        echo
         crontab -l
         exit 0
     fi
