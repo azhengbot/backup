1179a2e666800ceb2b479a775a075a0726d2733c
xuhaoqing <xuhaoqing@lattebank.com>
2021-09-01 14:46:29 +0800

    Backup on: 2021年 9月 1日 星期三 14时46分29秒 CST

---
+++ install_backup.sh
@@ -0,0 +1,39 @@
+# 全局变量
+WD=`pwd`
+# > 安装自动备份脚本到 contab
+install_to_crontab() {
+    script="bash $WD/backup.sh"
+    # echo $script
+    crontab -l > /tmp/crontab_conf
+    if grep -q "no crontab" /tmp/crontab_conf ; then
+        # echo /tmp/crontab_conf
+        # manual
+        echo "自动添加失败，请尝试手动添加: "
+        echo
+        echo SHELL=$SHELL > crontab_conf
+        echo PATH=$PATH >> crontab_conf
+        echo "00 12 * * * $script > /tmp/dotfile_Backup.log && osascript -e 'display notification \"$WD/backup\" with title \"dotfile备份成功, 已上传至 GitHub\" '" >> crontab_conf
+        echo "运行 \$ crontab -e"
+
+        echo "插入以下内容: "
+        cat crontab_conf
+        exit 1
+    else
+        # echo "hav crontab"
+        # auto
+        crontab -l > crontab_conf
+        echo SHELL=$SHELL >> crontab_conf
+        echo PATH=$PATH >> crontab_conf
+        echo "50 14 * * * $script > /tmp/dotfile_Backup.log && osascript -e 'display notification \"$WD/backup\" with title \"dotfile备份成功, 已上传至 GitHub\" '" >> crontab_conf
+        crontab crontab_conf && rm -f crontab_conf
+
+        echo "🎉 自动化备份dotfile安装完成"
+        echo
+        echo "目前的 crontab 配置"
+        echo
+        crontab -l
+        exit 0
+    fi
+}
+
+install_to_crontab
