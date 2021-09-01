09b6b3fb0b508557dac7a2c79f424e988a9faf88
xuhaoqing <xuhaoqing@lattebank.com>
2021-09-01 14:51:40 +0800

    Backup on: 2021年 9月 1日 星期三 14时51分40秒 CST

--- backup.sh
+++ backup.sh
@@ -8,12 +8,13 @@ backup_dotfiles(){
 
 backup_to_github(){
     msg='Backup on: '`date`
-    # echo $msg
+    echo $msg
 
     git add .
     git commit -m "$msg"
     git push --set-upstream origin main
     git push
+    echo "上传成功"
 }
 
 backup_dotfiles
