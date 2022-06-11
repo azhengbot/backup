6a7691c1b38af726263c4a83ec506ee0ee5bf568
xuhaoqing <xuhaoqing@lattebank.com>
2022-06-11 10:37:21 +0800

    Backup on: Sat Jun 11 10:37:21 CST 2022

--- backup.sh
+++ backup.sh
@@ -11,19 +11,18 @@ backup_dotfiles(){
     echo "复制成功"
 }
 
-backup_to_github(){
+backup_to_github() {
     msg='Backup on: '`date`
     echo $msg
 
-    git add $DIR
-    git commit -m "$msg"
-    git pull
-    a = 1 / 0
+    git add $DIR && \
+    git commit -m "$msg" && \
+    git pull && \
+    a = 1 / 0 && \
     git push --set-upstream origin main && \
-    echo "上传成功"
-} || {
+    echo "上传成功" || \
     osascript -e 'display notification "backup 备份失败" with title "backup 备份失败"'
-}
+} 
 
 # 主程序
 cd $DIR # 这个很重要
